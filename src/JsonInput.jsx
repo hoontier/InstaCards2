f1aa07d5659d4cbdd1adf1d8999a51c5d2c11428
hoontier <huntersutcliffe@hotmail.com>
2023-06-17 14:44:00 -0400

    submit button

--- src/JsonInput.jsx
+++ src/JsonInput.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 
-/* rewrite this file so that it receives text formatted as JSON data */
 export default function JsonInput({onJsonData}) {
   const [jsonText, setJsonText] = useState("");
 
+  //input change event handler
   const handleInputChange = (event) => {
     setJsonText(event.target.value);
+    }
+
+  //submit button click event handler
+  const handleSubmit = () => {
+    setJsonText(event.target.value);
     try {
-      // remove text before and after the [ and ] characters
-        const startIndex = event.target.value.indexOf("[");
-        const endIndex = event.target.value.lastIndexOf("]");
-        const trimmedText = event.target.value.substring(startIndex, endIndex + 1);
+        // remove text before and after the [ and ] characters
+        const startIndex = jsonText.indexOf("[");
+        const endIndex = jsonText.lastIndexOf("]");
+        const trimmedText = jsonText.substring(startIndex, endIndex + 1);
         const jsonData = JSON.parse(trimmedText);
         onJsonData(jsonData);
     } catch(e) {
@@ -18,9 +23,11 @@ export default function JsonInput({onJsonData}) {
     }
   }
 
+  //full card component
   return (
     <div className="jsonInput">
       <textarea value={jsonText} onChange={handleInputChange} placeholder = "enter JSON here" />
+      <button className="submit" onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
